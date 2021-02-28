8ce7144278679b8256341f20252e30544f7756df
Julián de Pablo <julidpt98@gmail.com>
2021-02-28 17:49:34 +0100

    mejorado navbar

--- proyectoDeComputacion/src/app/app.module.ts
+++ proyectoDeComputacion/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
