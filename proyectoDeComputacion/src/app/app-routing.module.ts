989a2a91293342e832ca6bb7158cd082a36a4ffd
Julián de Pablo <julidpt98@gmail.com>
2021-03-02 19:18:44 +0100

    mejorado routing con home como pagina de inicio

--- proyectoDeComputacion/src/app/app-routing.module.ts
+++ proyectoDeComputacion/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import {SentimentComponent} from './components/sentiment/sentiment.component';
 
 const routes: Routes = [
   {
-    path:'home',
+    path:'',
     component:HomeComponent
   },
   {
