abe0afc4970ba4d02e4d24c487faedf05ab58b4e
Julián de Pablo <julidpt98@gmail.com>
2021-03-03 23:38:08 +0100

    corregido routing

--- proyectoDeComputacion/src/app/app.module.ts
+++ proyectoDeComputacion/src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './components/home/home.component';
+import { SentimentComponent } from './components/sentiment/sentiment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SentimentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule,
     NgbModule
   ],
   providers: [],
