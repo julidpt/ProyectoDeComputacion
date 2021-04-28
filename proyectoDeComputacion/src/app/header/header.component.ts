9b59c7044417de446751a35857af3398a2b58b33
Julián de Pablo <julidpt98@gmail.com>
2021-04-28 17:37:03 +0200

    modified login and register

--- proyectoDeComputacion/src/app/header/header.component.ts
+++ proyectoDeComputacion/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
   exit(){
-    window.alert("¡Has cerrado sesión correctamente!");
+    alert("¡Has cerrado sesión correctamente!");
+    this.authService.userLogout();
   }
 
 }
--- proyectoDeComputacion/src/app/services/auth.service.ts
+++ proyectoDeComputacion/src/app/services/auth.service.ts
@@ -1,10 +1,7 @@
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { baseUrl } from 'src/environments/environment';
-import { BehaviorSubject } from 'rxjs';
-import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
