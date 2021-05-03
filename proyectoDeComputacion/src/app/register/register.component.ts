a34f21709d3f0a99f475710bdc71097acd085596
alfonsovega97 <alfonsovega1005@hotmail.com>
2021-05-03 13:47:16 +0200

    pequeños avances

--- proyectoDeComputacion/src/app/register/register.component.ts
+++ proyectoDeComputacion/src/app/register/register.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MustMatch} from '../utils/register.validator';
 
 @Component({
   selector: 'app-register',
@@ -12,8 +13,9 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 
 export class RegisterComponent implements OnInit {
   form = new FormGroup({
-    email: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
     password: new FormControl('', Validators.required),
+    confirm_password: new FormControl('', Validators.required),
     name: new FormControl('', Validators.required),
     surnames: new FormControl('', Validators.required),
     phone: new FormControl('', Validators.required),
@@ -21,9 +23,23 @@ export class RegisterComponent implements OnInit {
   json: any = false;
   alert: any;
 
+  checkIfMatchingPasswords(passwordKey: string, passwordConfirmationKey: string) {
+    return (group: FormGroup) => {
+      let passwordInput = group.controls[passwordKey],
+          passwordConfirmationInput = group.controls[passwordConfirmationKey];
+      if (passwordInput.value !== passwordConfirmationInput.value) {
+        return passwordConfirmationInput.setErrors({notEquivalent: true})
+      }
+      else {
+          return passwordConfirmationInput.setErrors(null);
+      }
+    }
+  }
+
   constructor(private http: HttpClient, private route: Router, private formBuilder: FormBuilder, private authService: AuthService) { }
 
   ngOnInit(): void {
+    
   }
 
   submit() {
