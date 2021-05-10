5c10ab142e8f8d2aba5b9c7784acb3217a7e1f74
Merle66 <Merle66>
2021-05-10 12:24:50 +0200

    Carrusel pagina principal
    Faltan cosas

--- proyectoDeComputacion/src/app/home/home.component.ts
+++ proyectoDeComputacion/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,3 +14,4 @@ export class HomeComponent implements OnInit {
   }
 
 }
+
