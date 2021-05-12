a2c802fb61e0c2396058ffac6bce51797f55e8e0
Merle66 <Merle66>
2021-05-12 17:59:35 +0200

    Pagina principal con links a cada pueblo
    Falta poner nuevas imagenes y definir la parte de town

--- proyectoDeComputacion/src/app/home/home.component.ts
+++ proyectoDeComputacion/src/app/home/home.component.ts
@@ -8,6 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  //Mas visitados
+  cards = [{ "imagen": "../../assets/towns/santillana.jpg", "nombre": "Santillana del Mar" },
+  { "imagen": "../../assets/towns/trujillo.jpg", "nombre": "Trujillo" },
+  { "imagen": "../../assets/towns/pedraza.jpg", "nombre": "Pedraza" },
+  { "imagen": "../../assets/towns/chinchon.jpg", "nombre": "Chinchón" },
+  { "imagen": "../../assets/hotel3.png", "nombre": "Hotel 1" },
+  { "imagen": "../../assets/hotel2.png", "nombre": "Hotel 2" }];
+
+  //Mejor valorados
+  cards2 = [{ "imagen": "../../assets/towns/santillana.jpg", "nombre": "Santillana del Mar" },
+  { "imagen": "../../assets/towns/trujillo.jpg", "nombre": "Trujillo" },
+  { "imagen": "../../assets/towns/pedraza.jpg", "nombre": "Pedraza" },
+  { "imagen": "../../assets/towns/chinchon.jpg", "nombre": "Chinchón" },
+  { "imagen": "../../assets/hotel1.png", "nombre": "Hotel 1" },
+  { "imagen": "../../assets/hotel2.png", "nombre": "Hotel 2" }];
+
+  //Rural
+  cards3 = [{ "imagen": "../../assets/towns/santillana.jpg", "nombre": "Santillana del Mar" },
+  { "imagen": "../../assets/towns/trujillo.jpg", "nombre": "Trujillo" },
+  { "imagen": "../../assets/towns/pedraza.jpg", "nombre": "Pedraza" },
+  { "imagen": "../../assets/towns/chinchon.jpg", "nombre": "Chinchón" },
+  { "imagen": "../../assets/hotel3.png", "nombre": "Hotel 1" },
+  { "imagen": "../../assets/hotel2.png", "nombre": "Hotel 2" }];
   constructor() { }
 
   ngOnInit(): void {
