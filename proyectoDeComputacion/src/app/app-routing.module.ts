05f292f3b5a3270fa04c19a537313fb1fec7397e
Juan Lasso de la Vega <45948576+lassete6898@users.noreply.github.com>
2021-06-11 19:52:39 +0200

    limpieza

--- proyectoDeComputacion/src/app/app-routing.module.ts
+++ proyectoDeComputacion/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { HomeComponent } from './home/home.component';
-import { SentimentComponent } from './sentiment/sentiment.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { TownComponent } from './town/town.component';
@@ -18,10 +17,6 @@ const routes: Routes = [
   {
     path: '', 
     component: HomeComponent
-  },
-  {
-    path: 'sentiment', 
-    component: SentimentComponent
   },
   {
     path: 'user',
--- proyectoDeComputacion/src/app/login/forgot-psswd/forgot-psswd.component.ts
+++ proyectoDeComputacion/src/app/login/forgot-psswd/forgot-psswd.component.ts
@@ -38,8 +38,4 @@ export class forgotPasswordComponent implements OnInit {
             this.badreq = true;
           })
     }
-  
-  
-
-
 }
